01ddd1dc8da7ab37fc1aeed7bd155222aed51b3a
adcdenis <adcdenis@gmail.com>
2020-07-07 19:23:24 -0300

    mais uma tentativa

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const favicon = require('express-favicon')
 const path = require('path')
-const port = process.env.PORT || 8090
+const port = process.env.PORT || 8080
 const app = express()
 app.use(favicon(__dirname + '/build/favicon.ico'))
 // the __dirname is the current directory from where the script is running
@@ -14,3 +14,4 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
+console.log(port)
